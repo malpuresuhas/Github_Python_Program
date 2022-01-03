3d5b2405b8e77289344ee9ac27be4c91df7b4bca
malpuresuhas <suhasmalpure@gmail.com>
2022-01-03 12:24:04 +0530

    printFunctionPython

---
+++ printFunctionExamples.py
@@ -0,0 +1,22 @@
+# Program to understand print function usage
+ethosspace = 'Testing Services'
+print (ethosspace)
+#output is ' Testing Services '
+
+a=9
+b='Testing'
+c=['Prog']
+print(a,b,c)
+#output is 9 Testing ['Prog']
+
+print(a,b,c,sep=' => ')
+#output is          9 => Testing => ['Prog']
+
+print ('ethosspace'); 
+print('leading software testing provider')
+#output is ethosspace
+#output is         leading software testing provider
+
+print ('ethosspace',end="");
+print(' leading software testing provider')
+#output is  ethosspace leading software testing provider
